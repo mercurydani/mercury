3d8428e03ca3ea010049dc6687e11a31d938b9ca
Пользователь 1 <1@SASK.LOCAL>
2022-10-15 12:10:25 +0400

    commet lake

---
+++ app/src/main/java/space/mercury/mercuryquiz/GameLevels.java
@@ -0,0 +1,75 @@
+package space.mercury.mercuryquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //начало
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);finish();
+                }catch (Exception e){
+
+                }
+                //конец
+            }
+        });
+         //кнопка первый уровень
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                 Intent intent = new Intent(GameLevels.this, Level1.class);
+                 startActivity(intent);finish();
+                }catch (Exception e){
+                    //пусто
+                }
+            }
+        });
+        //кнопка первый уровень
+
+
+
+
+
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+
+
+
+    }
+    //системная кнопка назад-начало
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);finish();
+        }catch (Exception e){
+
+        }
+    }
+    //системная кнопка назад-конец
+}
